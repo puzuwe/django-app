dcce9fdea9de61205079d3b5e220d0133d6e54a1
Patrick Uzuwe <puzuwe@yahoo.co.uk>
2021-01-20 09:08:25 +0000

    Initial Django App 2

--- django_app/settings.py
+++ django_app/settings.py
@@ -13,7 +13,7 @@ import os
 import sys
 import dj_database_url
 from pathlib import Path
-from django,core.management.utils import get_random_secret_key
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
